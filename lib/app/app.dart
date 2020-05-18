1c2d41d7f67d708c09dda6c94c4d904a2cd7d731
Xheghun <ysenruf@gmail.com>
2020-05-18 03:02:50 +0200

    initial setup

---
+++ lib/app/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:mariamproject/app/providers/base.dart';
+import 'package:mariamproject/app/screens/login_screen.dart';
+import 'package:provider/provider.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        //register providers
+        ChangeNotifierProvider.value(value: BaseProvider()),
+      ],
+      child: MaterialApp(
+        home: LoginScreen(),
+        theme: ThemeData(
+          fontFamily: "Comfortaa-Regular",
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        routes: <String, WidgetBuilder>{
+          LoginScreen.routeName: (context) => LoginScreen(),
+        },
+      ),
+    );
+  }
+}
---
+++ lib/app/screens/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  //declare route name for navigation reference
+  static final String routeName = "login_screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Text(""),
+        ],
+      ),
+    );
+  }
+}
